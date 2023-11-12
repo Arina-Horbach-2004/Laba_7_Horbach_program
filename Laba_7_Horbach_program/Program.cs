4a21c50c03d736472f7d9ef76a903b7952ac851c
Arina Gorbach <a.a.horbach@student.khai.edu>
2023-11-12 18:25:25 +0100

    Добавьте файлы проекта.

---
+++ Laba_7_Horbach_program/Program.cs
@@ -0,0 +1,142 @@
+﻿using Laba_7_Horbach_program;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Введіть максимальну кількість користувачів (N): ");
+        if (int.TryParse(Console.ReadLine(), out int max_objects) && max_objects > 0)
+        {
+            Console.WriteLine($"Максимальна кількість користувачів: {max_objects}");
+
+            List<Person> people = new();
+
+            while (true)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1 - Додати об’єкт");
+                Console.WriteLine("2 - Вивести на екран об’єкти");
+                Console.WriteLine("3 - Знайти об’єкт");
+                Console.WriteLine("4 - Видалити об’єкт");
+                Console.WriteLine("5 - Демонстрація поведінки об’єктів");
+                Console.WriteLine("6 - Демонстрація роботи static методів");
+                Console.WriteLine("7 - зберегти колекцію об’єктів у файлі");
+                Console.WriteLine("8 - зчитати колекцію об’єктів з файлу");
+                Console.WriteLine("9 - очистити колекцію об’єктів");
+                Console.WriteLine("0 - Вийти з програми");
+                Console.Write("Виберіть опцію: ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    try
+                    {
+                        switch (choice)
+                        {
+                            case 1:
+                                Add_Person(people, max_objects);
+                                break;
+
+                            case 2:
+                                Display_People(people);
+                                break;
+
+                            case 3:
+                                Find_Person(people);
+                                break;
+
+                            case 4:
+                                Remove_Person(people);
+                                break;
+
+                            case 5:
+                                Demo_behavior(people);
+                                break;
+
+                            case 6:
+                                DemoStaticMethods(people);
+                                break;
+                            case 7:
+                                Console.WriteLine("1 – зберегти у файл *.csv (*.txt)");
+                                Console.WriteLine("2 – зберегти у файл *.json");
+                                int save = int.Parse(Console.ReadLine());
+                                switch (save)
+                                {
+                                    case 1:
+
+                                        string csvFilePath = @"C:\Users\Arina Gorbach\Desktop\Lab_7.csv";
+                                        SaveToFIleCVS(people, csvFilePath);
+                                        Console.WriteLine("Data loaded into a CSV file.");
+                                        break;
+                                    case 2:
+                                        string jsonFilePath = @"C:\Users\Arina Gorbach\Desktop\Lab_7.json";
+                                        SaveToFileJson(people, jsonFilePath);
+                                        Console.WriteLine("Data loaded into JSON file.");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Некоректний вибір способу зберігання.");
+                                        break;
+                                }
+                                break;
+                            case 8:
+                                Console.WriteLine("1 – зчитати з файлу *.csv (*.txt)");
+                                Console.WriteLine("2 – зчитати з файлу *.json");
+                                int read = int.Parse(Console.ReadLine());
+                                switch (read)
+                                {
+                                    case 1:
+                                        string readCVSFile = @"C:\Users\Arina Gorbach\Desktop\Lab_7.csv";
+                                        people = ReadFromFileCSV(readCVSFile);
+                                        Console.WriteLine("Data loaded from CSV file.");
+                                        break;
+                                    case 2:
+                                        string readJsonFile = @"C:\Users\Arina Gorbach\Desktop\Lab_7.json";
+                                        people = ReadFromFileJson(readJsonFile);
+                                        Console.WriteLine("Data loaded from JSON file.");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Некоректний вибір способу завантаження.");
+                                        break;
+                                }
+                                break;
+                            case 9:
+                                people.Clear();
+                                Console.WriteLine("Колекція очищена");
+                                break;
+
+                            case 0:
+                                Console.WriteLine("Завершення роботи програми.");
+                                return;
+
+                            default:
+                                Console.WriteLine("Некоректний вибір опції.");
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Помилка: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Некоректний вибір опції.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення для максимальної кількості користувачів.");
+        }
+    }
+
+
+}
---
+++ TestProject1/ProgramTest.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Laba_7_Horbach_program;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Test_Add_Person_With_Default_Constructor()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+            using (StringReader sr = new StringReader("1\n1\n"))
+            {
+                Console.SetIn(sr);
+                Program.Add_Person(people, maxObjects);
+            }
+
+            // Assert
+            Assert.AreEqual(1, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_Specific_Name_And_Gender()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+            Console.SetIn(new System.IO.StringReader("1\n2\nAlice\nFemale\n"));
+            global::Program.Add_Person(people, maxObjects);
+
+            // Assert
+            Assert.AreEqual(1, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_Invalid_Gender()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+            Console.SetIn(new System.IO.StringReader("1\n2\nAlice\nInvalidGender\n"));
+            global::Program.Add_Person(people, maxObjects);
+
+            // Assert
+            Assert.AreEqual(0, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_All_Parametry()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+
+            using (StringReader sr = new StringReader("1\n3\nJohn\nMale\n01.01.1990\n48538626501\n"))
+            {
+                Console.SetIn(sr);
+
+                // Act
+                Program.Add_Person(people, maxObjects);
+
+                // Assert
+                Assert.AreEqual(1, people.Count);
+                // Перевірка інших атрибутів створеного об'єкта (ім'я, стать, дата народження, номер телефону)
+                var addedPerson = people[0];
+                Assert.AreEqual("John", addedPerson.Name);
+                Assert.AreEqual(Gender.Male, addedPerson.Gender);
+                Assert.AreEqual(new DateTime(1990, 1, 1), addedPerson.BirthDate);
+                Assert.AreEqual("48538626501", addedPerson.Number);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_Invalid_BirthDate()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            using (StringReader sr = new StringReader("1\n3\nSeyran\nFemale\nInvaliedBirthDate\n48538626501\n"))
+            {
+                Console.SetIn(sr);
+
+                // Act
+                Program.Add_Person(people, maxObjects);
+
+                // Assert
+                Assert.AreEqual(0, people.Count);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_Invalid_Input()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+
+            using (StringReader sr = new StringReader("1\n3\nFerit\nmale\nInvalidBirthDate\n48538602101\n"))
+            {
+                Console.SetIn(sr);
+                Program.Add_Person(people, maxObjects);
+            }
+
+            // Assert
+            Assert.AreEqual(0, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_User_Input_String()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+            Console.SetIn(new System.IO.StringReader("2\nAlice,Female,01.01.1990,48538353801\n"));
+            global::Program.Add_Person(people, maxObjects);
+
+            // Assert
+            Assert.AreEqual(1, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Add_Person_With_Invalid_User_Input_String()
+        {
+            // Arrange
+            List<Person> people = new List<Person>();
+            int maxObjects = 5;
+
+            // Act
+            Console.SetIn(new System.IO.StringReader("2\nInvalidInput\n"));
+            global::Program.Add_Person(people, maxObjects);
+
+            // Assert
+            Assert.AreEqual(0, people.Count);
+        }
+
+        [TestMethod]
+        public void Test_Find_Person_By_Name()
+        {
+            // Arrange
+            List<Person> people = new List<Person>
+            {
+                new Person("John", Gender.Male),
+                new Person("Alice", Gender.Female)
+            };
+            string expectedName = "John";
+
+            // Act
+            using (StringReader sr = new StringReader($"Name\n{expectedName}\n"))
+            {
+                Console.SetIn(sr);
+                using (StringWriter sw = new StringWriter())
+                {
+                    Console.SetOut(sw);
+                    Program.Find_Person(people);
+                    string output = sw.ToString();
+                    // Assert
+                    Assert.IsTrue(output.Contains("Результат пошуку:"));
+                    Assert.IsTrue(output.Contains(expectedName));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_Find_Person_Not_Found()
+        {
+            // Arrange
+            List<Person> people = new List<Person>
+            {
+                new Person("John", Gender.Male),
+                new Person("Alice", Gender.Female)
+            };
+            string searchValue = "UnknownName";
+
+            // Act
+            using (StringReader sr = new StringReader($"Name\n{searchValue}\n"))
+            {
+                Console.SetIn(sr);
+                using (StringWriter sw = new StringWriter())
+                {
+                    Console.SetOut(sw);
+                    Program.Find_Person(people);
+                    string output = sw.ToString();
+                    // Assert
+                    Assert.IsTrue(output.Contains("Об'єкт, який відповідає умові пошуку, не знайдено."));
+                }
+            }
+        }
+    }
+}
